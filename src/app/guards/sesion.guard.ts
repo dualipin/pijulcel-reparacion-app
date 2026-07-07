2797dc4bc2086c13de2eef52dc4f680c5d99379d
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2026-07-07 12:30:54 -0600

    refactor: Improve token retrieval logic in sesionGuard by adding storage service integration

--- src/app/guards/sesion.guard.ts
+++ src/app/guards/sesion.guard.ts
@@ -2,11 +2,21 @@ import { NavController } from '@ionic/angular/standalone';
 import { CanActivateFn } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { StorageService } from '../services/storage.service';
 
 export const sesionGuard: CanActivateFn = async (route, state) => {
   const authServ = inject(AuthService);
+  const storageServ = inject(StorageService);
   const navCtrl = inject(NavController);
-  const token = authServ.token;
+  
+  let token = authServ.token;
+  if (!token) {
+    token = await storageServ.getItemStorage('token');
+    if (token) {
+      authServ.token = token;
+    }
+  }
+
   console.log('Token en guard:', token);
   if (token == null || token === '') {
     navCtrl.navigateRoot('/login');
